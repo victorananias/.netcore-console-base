b268312196eed04f9592791037c66e1afc504560
Victor <victor@ananias.dev>
2019-10-02 15:52:56 -0300

    Mail class updated

--- Mail.cs
+++ Mail.cs
@@ -2,14 +2,17 @@ using System;
 using System.Net;
 using System.Net.Mail;
 
-namespace Classes
+namespace DCGAvisoMultiCD.Src
 {
-    class Mail
+    public class Mail
     {
         private string _host;
         private int _port;
         private string _username;
         private string _password;
+        private string _cc;
+        private string _bcc;
+        private string _to;
 
         public Mail (string host, int port, string username, string password)
         {
@@ -19,7 +22,26 @@ namespace Classes
             _password = password;
         }
 
-        public bool Send (string from, string to, string subject, string textMessage)
+        public Mail CC(string cc)
+        {
+            _cc = cc;
+            return this;
+        }
+
+
+        public Mail BCC(string bcc)
+        {
+            _bcc = bcc;
+            return this;
+        }
+
+        public Mail To(string to)
+        {
+            _to = to;
+            return this;
+        }
+
+        public bool Send (string subject, string body, bool isBodyHtml = true)
         {
             try
             {
@@ -27,12 +49,28 @@ namespace Classes
                     client.EnableSsl = true;
                     client.Credentials = new NetworkCredential (_username, _password);
 
-                    MailMessage message = new MailMessage (
-                        from,
-                        to,
-                        subject,
-                        textMessage
-                    );
+                    MailMessage message = new MailMessage ();
+                    
+                    message.From = new MailAddress(_username);
+                    message.Subject = subject;
+                    message.Body = body;
+
+                    foreach (var address in _to.Split(new [] {";"}, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        message.To.Add(address);    
+                    }
+
+                    foreach (var address in _cc.Split(new [] {";"}, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        message.CC.Add(address);    
+                    }
+
+                    foreach (var address in _bcc.Split(new [] {";"}, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        message.Bcc.Add(address);    
+                    }
+
+                    message.IsBodyHtml = isBodyHtml;
 
                     client.Send (message);
 
