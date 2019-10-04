e6200badbefb9fa34ff2e4ab9173552fed1eeefe
Victor <victor@ananias.dev>
2019-10-04 09:03:17 -0300

    Logger class added

---
+++ Logger.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Classes
+{
+    public class Logger
+    {
+        private string _logsDirectory;
+        private string _logFile;
+
+        public Logger(IConfiguration _config)
+        {
+            _logsDirectory = string.IsNullOrEmpty(_config.GetSection("Logger")["Path"]) ? "./logs": _config.GetSection("Logger")["Path"];
+            _logFile = _logsDirectory + "/" + DateTime.Now.ToString("yyyyMMdd");
+        }
+
+        public void Create (string log)
+        {
+            CheckLogsDirectory();
+
+            Console.WriteLine(log);
+
+            Insert(log);
+        }
+
+        private void Insert(string log)
+        {
+            using (StreamWriter fileStream = new StreamWriter(_logFile, true))
+            {
+                fileStream.WriteLine("---"+ DateTime.Now.ToString("HH:mm:ss") + "---");
+                fileStream.WriteLine(log);
+                fileStream.Close();
+            }
+        }
+
+        public void CheckLogsDirectory()
+        {
+            if (!Directory.Exists (_logsDirectory)) {
+                Directory.CreateDirectory(_logsDirectory);
+            }
+        }
+    }
+}
