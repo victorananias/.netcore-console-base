556a533e503c77aee7ed61607ceb24646d81528a
Victor <victor.ananias8@gmail.com>
2020-04-03 09:58:13 -0300

    logging to file added

--- ConsoleApp/Services/MainService.cs
+++ ConsoleApp/Services/MainService.cs
@@ -2,17 +2,20 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Logging;
 
 
 namespace ConsoleApp.Services
 {
     public class MainService : IMainService
     {
-        private IHostApplicationLifetime _appLifetime;
+        private readonly IHostApplicationLifetime _appLifetime;
+        private ILogger<MainService> _logger;
 
-        public MainService(IHostApplicationLifetime appLifetime)
+        public MainService(IHostApplicationLifetime appLifetime, ILogger<MainService> logger)
         {
             _appLifetime = appLifetime;
+            _logger = logger;
         }
 
         public async Task Run()
