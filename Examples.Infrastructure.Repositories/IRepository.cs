9d018ceb9f4091640af44a3a0a31c7d34429daab
Victor <victor.ananias8@gmail.com>
2020-11-25 10:30:02 -0300

    Examples.Infrastructure.Repositories added

---
+++ Examples.Infrastructure.Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Infrastructure.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllByWhereAsync(Expression<Func<T, bool>> where);
+        Task SaveAsync(T entity);
+        Task SaveAsync(IEnumerable<T> list);
+        Task DeleteAsync(T entity);
+        Task DeleteAsync(IEnumerable<T> list);
+        Task UpdateAsync(T entity);
+        Task UpdateAsync(IEnumerable<T> list);
+        Task<int> GetCountAsync();
+    }
+}
---
+++ Examples.Infrastructure.Repositories/Repository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext _dbContext;
+        protected readonly DbSet<T> _table;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _table = _dbContext.Set<T>();
+        }
+        public async Task DeleteAsync(T entity)
+        {
+            _table.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(IEnumerable<T> list)
+        {
+            _table.RemoveRange(list);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllByWhereAsync(Expression<Func<T, bool>> where)
+        {
+            return _table.Where(where);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return _table.Find(id);
+        }
+
+        public async Task<int> GetCountAsync()
+        {
+            return await _table.CountAsync();
+        }
+
+        public async Task SaveAsync(T entity)
+        {
+            _table.Add(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task SaveAsync(IEnumerable<T> list)
+        {
+            _table.AddRange(list);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _table.Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(IEnumerable<T> list)
+        {
+            _table.UpdateRange(list);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
